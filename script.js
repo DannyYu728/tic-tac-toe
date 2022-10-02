07709ff4cdf1ec685214f2bcaf5b2eab1e83577a
Danny Yu <tiramisu@Dannys-MBP.home>
2022-10-01 22:38:59 -0400

    check Winner

--- script.js
+++ script.js
@@ -12,9 +12,31 @@ let grid = new Map([
 ])
 let turn = 0
 
+let winnerCheck = (map) => {
+  if ((map.get("tL") === 1 && map.get("tM") === 1 && map.get("tR") === 1)
+  || (map.get("mL") === 1 && map.get("mM") === 1 && map.get("mR") === 1)
+  || (map.get("bL") === 1 && map.get("bM") === 1 && map.get("bR") === 1)
+  || (map.get("tL") === 1 && map.get("mL") === 1 && map.get("bL") === 1)
+  || (map.get("tM") === 1 && map.get("mM") === 1 && map.get("bM") === 1)
+  || (map.get("tR") === 1 && map.get("mR") === 1 && map.get("bR") === 1)
+  || (map.get("bL") === 1 && map.get("mM") === 1 && map.get("tR") === 1) ||
+  (map.get("mM") === 1 && map.get("tL") === 1 && map.get("bR") === 1)) {
+  console.log("Player 1 Wins")
+  }
+  else if ((map.get("tL") === 2 && map.get("tM") === 2 && map.get("tR") === 2)
+  || (map.get("mL") === 2 && map.get("mM") === 2 && map.get("mR") === 2)
+  || (map.get("bL") === 2 && map.get("bM") === 2 && map.get("bR") === 2)
+  || (map.get("tL") === 2 && map.get("mL") === 2 && map.get("bL") === 2)
+  || (map.get("tM") === 2 && map.get("mM") === 2 && map.get("bM") === 2)
+  || (map.get("tR") === 2 && map.get("mR") === 2 && map.get("bR") === 2)
+  || (map.get("bL") === 2 && map.get("mM") === 2 && map.get("tR") === 2) ||
+  (map.get("mM") === 2 && map.get("tL") === 2 && map.get("bR") === 2)) {
+  console.log("Player 2 Wins")
+  }
+}
+
 let boxMaker = (map) => {
   map.forEach((i, location) => {
-
     let htmlTemplate = `
     <div class ="box ${location}">
     </div>`
@@ -26,19 +48,20 @@ let boxMaker = (map) => {
           map.set(`${location}`, 1)
           box.style.backgroundColor = "red"
           turn++
+          winnerCheck(map)
         }
       } else {
         if (map.get(`${location}`) == 0) {
           map.set(`${location}`, 2)
           box.style.backgroundColor = "blue"
           turn++
+          winnerCheck(map)
         }
       }
     }))
   })
 }
 boxMaker(grid)
-
 
 
 
