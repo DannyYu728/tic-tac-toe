a2d38dca7e1d486016c64285d4e5f06d14586f87
Danny Yu <tiramisu@Dannys-MBP.home>
2022-10-01 21:05:44 -0400

    draft 2

--- script.js
+++ script.js
@@ -1,37 +1,44 @@
 let game = document.querySelector(".game")
 let grid = new Map([
-  ["topLeft", 0],
-  ["topMid", 0],
-  ["topRight", 0],
-  ["midLeft", 0],
-  ["midMid", 0],
-  ["midRight", 0],
-  ["botLeft", 0],
-  ["botMid", 0],
-  ["botRight", 0],
+  ["tL", 0],
+  ["tM", 0],
+  ["tR", 0],
+  ["mL", 0],
+  ["mM", 0],
+  ["mR", 0],
+  ["bL", 0],
+  ["bM", 0],
+  ["bR", 0],
 ])
-let turn = 1
+let turn = 0
 
 let boxMaker = (map) => {
-  map.forEach((status, location) => {
+  map.forEach((i, location) => {
+
     let htmlTemplate = `
     <div class ="box ${location}">
     </div>`
     game.insertAdjacentHTML("beforeend", htmlTemplate)
     let boxes = document.querySelectorAll(`.${location}`)
     boxes.forEach((box) => box.addEventListener("click", function () {
-      if (turn = even)
-      
-      if (map.get(`${location}`) == 0) {
-        map.set(`${location}`, 1)
-        box.style.backgroundColor = "red"
-        console.log(map)
+      if (turn % 2 == 0) {
+        if (map.get(`${location}`) == 0) {
+          map.set(`${location}`, 1)
+          box.style.backgroundColor = "red"
+          turn++
+        }
       } else {
-        map.set(`${location}`, 0)
-        box.style.backgroundColor = "blue"
-        console.log(map)
+        if (map.get(`${location}`) == 0) {
+          map.set(`${location}`, 2)
+          box.style.backgroundColor = "blue"
+          turn++
+        }
       }
     }))
   })
 }
 boxMaker(grid)
+
+
+
+
