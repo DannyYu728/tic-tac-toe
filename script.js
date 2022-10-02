52a50b0d3f4f7b6373af671591f27660890209b2
Danny Yu <tiramisu@Dannys-MBP.home>
2022-10-02 01:02:28 -0400

    timer and annouce

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let right = document.querySelector(".right")
 let play1Score = document.querySelector(".play1")
 let play2Score = document.querySelector(".play2")
 let playTurn = document.querySelector("h3")
+let annouce = document.querySelector("h4")
 let turn = 0
 let round = 0
 let player1Score = 0
@@ -48,9 +49,10 @@ let nextRound = (map) => {
   map.forEach(function (i, location) {
     map.set(`${location}`, 0)
     game.innerHTML = ""
+    annouce.innerText = ""
     // turn = 0
     round += 1
-    boxMaker(grid)
+    boxMaker(map)
   })
 }
 
@@ -65,7 +67,8 @@ let winnerCheck = (map) => {
     (map.get("mM") === 1 && map.get("tL") === 1 && map.get("bR") === 1)) {
     player1Score += 1
     play1Score.innerText = player1Score
-    nextRound(map)
+    annouce.textContent = "Player 1 WINS!"
+    setTimeout(nextRound, 1500, map)
   }
   else if ((map.get("tL") === 2 && map.get("tM") === 2 && map.get("tR") === 2)
     || (map.get("mL") === 2 && map.get("mM") === 2 && map.get("mR") === 2)
@@ -77,12 +80,14 @@ let winnerCheck = (map) => {
     (map.get("mM") === 2 && map.get("tL") === 2 && map.get("bR") === 2)) {
     player2Score += 1
     play2Score.innerText = player2Score
-    nextRound(map)
+    annouce.textContent = "Player 2 WINS!"
+    setTimeout(nextRound, 1500, map)
   } else if ((map.get("tL") != 0 && map.get("tM") != 0 && map.get("tR") != 0 &&
-  map.get("mL") != 0 && map.get("mM") != 0 && map.get("mR") != 0 &&
-  map.get("bL") != 0 && map.get("bM") != 0 && map.get("bR") != 0)) {
-    nextRound(map)
-}
+    map.get("mL") != 0 && map.get("mM") != 0 && map.get("mR") != 0 &&
+    map.get("bL") != 0 && map.get("bM") != 0 && map.get("bR") != 0)) {
+    annouce.textContent = "TIE!"
+    setTimeout(nextRound, 1500, map)
+  }
 }
 
 let boxMaker = (map) => {
